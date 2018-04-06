046d30155e526da9f3891d91c63aece85cb0f6b1
Rémi VEZY <vezy.remi@gmail.com>
2018-04-06 11:18:38 +0200

    Fix negative offer problem
    Add a maintenance respiration mortality (deficit in carbon allocation for each organ, computed as the allocation with negative offer).
    Add total mortality for each compartment.

--- R/2-Tree_DCM.R
+++ R/2-Tree_DCM.R
@@ -141,12 +141,53 @@ Shade.Tree= function(S,i){
   # also be computed first, actually
   S$Sim$Offer_Total_Tree[i]=
     S$Sim$GPP_Tree[i]-S$Sim$Rm_Tree[i]+S$Sim$Consumption_RE_Tree[i]
-  # If the offer is negative, there is mortality
+  # If the offer is negative (Rm>GPP+RE), there is mortality. This mortality is shared between
+  # the organs according to their potential carbon allocation (it is a deficit in carbon allocation)
 
-  #### Stem ####
-  # Offer: NB, Rm is used from the previous i, assumed not very different but could also be computed first, actually
+  if(S$Sim$Offer_Total_Tree[i]<0){
+    # Make it positive to cumulate in mortality:
+    S$Sim$Offer_Total_Tree[i]= -S$Sim$Offer_Total_Tree[i]
+    # Allocate carbon deficit to each organ:
+    S$Sim$M_Rm_Stem_Tree[i]=
+      S$Parameters$lambda_Stem_Tree*S$Sim$Offer_Total_Tree[i]
+    S$Sim$M_Rm_CR_Tree[i]=
+      S$Parameters$lambda_CR_Tree*S$Sim$Offer_Total_Tree[i]
+    S$Sim$M_Rm_Branch_Tree[i]=
+      S$Parameters$lambda_BranchWood_Tree*S$Sim$Offer_Total_Tree[i]
+    S$Sim$M_Rm_Leaf_Tree[i]=
+      min(S$Parameters$Demand_Leaf_Tree*S$Sim$Stocking_Tree[i],
+          S$Parameters$lambda_Leaf_Tree*S$Sim$Offer_Total_Tree[i])
+    S$Sim$M_Rm_FRoot_Tree[i]=
+      S$Parameters$lambda_FRoot_Tree*S$Sim$Offer_Total_Tree[i]
+    S$Sim$M_Rm_Reserves_Tree[i]=
+      S$Sim$Offer_Total_Tree[i]-
+      (S$Sim$M_Rm_FRoot_Tree[i]+S$Sim$M_Rm_Leaf_Tree[i]+
+         S$Sim$M_Rm_Branch_Tree[i]+S$Sim$M_Rm_CR_Tree[i]+
+         S$Sim$M_Rm_Stem_Tree[i])
+    # NB : M_Rm_Reserves_Tree is regarded as an extra reserve consumption as offer is not met.
+    S$Sim$Offer_Total_Tree[i]= 0
+  }
+
+  # Allocation to each compartment :
   S$Sim$Alloc_Stem_Tree[i]=
     S$Parameters$lambda_Stem_Tree*S$Sim$Offer_Total_Tree[i]
+  S$Sim$Alloc_CR_Tree[i]=
+    S$Parameters$lambda_CR_Tree*S$Sim$Offer_Total_Tree[i]
+  S$Sim$Alloc_Branch_Tree[i]=
+    S$Parameters$lambda_BranchWood_Tree*S$Sim$Offer_Total_Tree[i]
+  S$Sim$Alloc_Leaf_Tree[i]=
+    min(S$Parameters$Demand_Leaf_Tree*S$Sim$Stocking_Tree[i],
+        S$Parameters$lambda_Leaf_Tree*S$Sim$Offer_Total_Tree[i])
+  S$Sim$Alloc_FRoot_Tree[i]=
+    S$Parameters$lambda_FRoot_Tree*S$Sim$Offer_Total_Tree[i]
+  # Allocation to reserves (Offer - all other allocations):
+  S$Sim$Alloc_Reserves_Tree[i]=
+    S$Sim$Offer_Total_Tree[i]-
+    (S$Sim$Alloc_FRoot_Tree[i]+S$Sim$Alloc_Leaf_Tree[i]+
+       S$Sim$Alloc_Branch_Tree[i]+S$Sim$Alloc_CR_Tree[i]+
+       S$Sim$Alloc_Stem_Tree[i])
+
+  #### Stem ####
   #NPP = Offer * growth cost coefficient:
   S$Sim$NPP_Stem_Tree[i]=
     S$Parameters$epsilon_Stem_Tree*S$Sim$Alloc_Stem_Tree[i]
@@ -158,9 +199,6 @@ Shade.Tree= function(S,i){
 
 
   #### Coarse Roots ####
-  # Offer:
-  S$Sim$Alloc_CR_Tree[i]=
-    S$Parameters$lambda_CR_Tree*S$Sim$Offer_Total_Tree[i]
   #NPP
   S$Sim$NPP_CR_Tree[i]= S$Parameters$epsilon_CR_Tree*
     S$Sim$Alloc_CR_Tree[i]
@@ -173,9 +211,6 @@ Shade.Tree= function(S,i){
 
 
   #### Branches ####
-  # NB: Served first as Erythrina must regrow branches in priority after pruning
-  S$Sim$Alloc_Branch_Tree[i]=
-    S$Parameters$lambda_BranchWood_Tree*S$Sim$Offer_Total_Tree[i]
   #NPP
   S$Sim$NPP_Branch_Tree[i]=
     S$Parameters$epsilon_Branch_Tree*S$Sim$Alloc_Branch_Tree[i]
@@ -188,10 +223,6 @@ Shade.Tree= function(S,i){
 
 
   #### Leaves ####
-  # Offer:
-  S$Sim$Alloc_Leaf_Tree[i]=
-    min(S$Parameters$Demand_Leaf_Tree*S$Sim$Stocking_Tree[i],
-        S$Parameters$lambda_Leaf_Tree*S$Sim$Offer_Total_Tree[i])
   #NPP
   S$Sim$NPP_Leaf_Tree[i]=
     S$Parameters$epsilon_Leaf_Tree*S$Sim$Alloc_Leaf_Tree[i]
@@ -215,9 +246,6 @@ Shade.Tree= function(S,i){
   }
 
   #### Fine roots ####
-  # Offer
-  S$Sim$Alloc_FRoot_Tree[i]=
-    S$Parameters$lambda_FRoot_Tree*S$Sim$Offer_Total_Tree[i]
   # NPP
   S$Sim$NPP_FRoot_Tree[i]=
     S$Parameters$epsilon_FRoot_Tree*S$Sim$Alloc_FRoot_Tree[i]
@@ -230,14 +258,6 @@ Shade.Tree= function(S,i){
 
 
   #### Reserves ####
-
-  # Allocation to reserves (Offer - all other allocations)
-  S$Sim$Alloc_Reserves_Tree[i]=
-    S$Sim$Offer_Total_Tree[i]-
-    (S$Sim$Alloc_FRoot_Tree[i]+S$Sim$Alloc_Leaf_Tree[i]+
-       S$Sim$Alloc_Branch_Tree[i]+S$Sim$Alloc_CR_Tree[i]+
-       S$Sim$Alloc_Stem_Tree[i])
-
   S$Sim$NPP_Reserves_Tree[i]=
     S$Parameters$epsilon_Reserves_Tree*S$Sim$Alloc_Reserves_Tree[i]
   # Cost of allocating to reserves
@@ -294,33 +314,45 @@ Shade.Tree= function(S,i){
       S$Sim$CM_FRoot_Tree[previous_i(i,1)]*S$Parameters$RateThinning_Tree
   }
 
+
+  # Mortality update --------------------------------------------------------
+
+  S$Sim$Mortality_Leaf_Tree[i]=
+    S$Sim$M_Rm_Leaf_Tree[i]+S$Sim$Mact_Leaf_Tree[i]+S$Sim$MThinning_Leaf_Tree[i]
+  S$Sim$Mortality_Branch_Tree[i]=
+    S$Sim$M_Rm_Branch_Tree[i]+S$Sim$Mact_Branch_Tree[i]+S$Sim$MThinning_Branch_Tree[i]
+  S$Sim$Mortality_Stem_Tree[i]=
+    S$Sim$M_Rm_Stem_Tree[i]+S$Sim$Mact_Stem_Tree[i]+S$Sim$MThinning_Stem_Tree[i]
+  S$Sim$Mortality_CR_Tree[i]=
+    S$Sim$M_Rm_CR_Tree[i]+S$Sim$Mact_CR_Tree[i]+S$Sim$MThinning_CR_Tree[i]
+  S$Sim$Mortality_FRoot_Tree[i]=
+    S$Sim$M_Rm_FRoot_Tree[i]+S$Sim$Mact_FRoot_Tree[i]+
+    S$Sim$MThinning_FRoot_Tree[i]
+
+
   # Dry Mass update ---------------------------------------------------------
 
   S$Sim$CM_Leaf_Tree[i]=
-    S$Sim$CM_Leaf_Tree[previous_i(i,1)]+S$Sim$NPP_Leaf_Tree[i]-
-    S$Sim$Mact_Leaf_Tree[i]-S$Sim$MThinning_Leaf_Tree[i]
+    S$Sim$CM_Leaf_Tree[previous_i(i,1)]+S$Sim$NPP_Leaf_Tree[i]-S$Sim$Mortality_Leaf_Tree[i]
 
   S$Sim$CM_Branch_Tree[i]=
     S$Sim$CM_Branch_Tree[previous_i(i,1)]+S$Sim$NPP_Branch_Tree[i]-
-    S$Sim$Mact_Branch_Tree[i]-S$Sim$MThinning_Branch_Tree[i]
+    S$Sim$Mortality_Branch_Tree[i]
 
   S$Sim$CM_Stem_Tree[i]=
-    S$Sim$CM_Stem_Tree[previous_i(i,1)]+S$Sim$NPP_Stem_Tree[i]-
-    S$Sim$Mact_Stem_Tree[i]-S$Sim$MThinning_Stem_Tree[i]
+    S$Sim$CM_Stem_Tree[previous_i(i,1)]+S$Sim$NPP_Stem_Tree[i]-S$Sim$Mortality_Stem_Tree[i]
 
   S$Sim$CM_CR_Tree[i]=
     S$Sim$CM_CR_Tree[previous_i(i,1)]+
-    S$Sim$NPP_CR_Tree[i]- S$Sim$Mact_CR_Tree[i]-
-    S$Sim$MThinning_CR_Tree[i]
+    S$Sim$NPP_CR_Tree[i]-S$Sim$Mortality_CR_Tree[i]
 
   S$Sim$CM_FRoot_Tree[i]=
     S$Sim$CM_FRoot_Tree[previous_i(i,1)]+
-    S$Sim$NPP_FRoot_Tree[i]-S$Sim$Mact_FRoot_Tree[i]-
-    S$Sim$MThinning_FRoot_Tree[i]
+    S$Sim$NPP_FRoot_Tree[i]-S$Sim$Mortality_FRoot_Tree[i]
 
   S$Sim$CM_RE_Tree[i]=
     S$Sim$CM_RE_Tree[previous_i(i,1)]+
-    S$Sim$NPP_Reserves_Tree[i]-S$Sim$Consumption_RE_Tree[i]
+    S$Sim$NPP_Reserves_Tree[i]-S$Sim$Consumption_RE_Tree[i]-S$Sim$M_Rm_Reserves_Tree[i]
 
   ##########################################
   S$Sim$DM_Leaf_Tree[i]=
@@ -342,7 +374,6 @@ Shade.Tree= function(S,i){
   S$Sim$Ra_Tree[i]=
     S$Sim$Rm_Tree[i]+S$Sim$Rc_Tree[i]
 
-  # NPP_Tree
   S$Sim$NPP_Tree[i]=
     S$Sim$NPP_Stem_Tree[i]+S$Sim$NPP_Branch_Tree[i]+
     S$Sim$NPP_Leaf_Tree[i]+S$Sim$NPP_CR_Tree[i]+
