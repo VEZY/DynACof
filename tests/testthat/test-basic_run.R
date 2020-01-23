f1ddcc30b876ac95ec67fb70ece0b09f7e9e08e4
remi.vezy <VEZY@users.noreply.github.com>
2020-01-23 17:55:24 +0100

    Update test-basic_run.R

--- tests/testthat/test-basic_run.R
+++ tests/testthat/test-basic_run.R
@@ -1,7 +1,7 @@
 context("Test simple examples")
 
 Sys.setenv(TZ="UTC")
-test_that("DynACof example works", {
+test_that("DynACof runs for full sun", {
   # DynACof::write.results(DynACof(Period= as.POSIXct(c("1979-01-01", "1980-01-02")),
   #                         Outpath = "tests/testthat")
   ref_sim= readRDS("ref_sim.RData")
